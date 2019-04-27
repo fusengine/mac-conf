8bf21e1d5b76f07fb9f48eca2218fc62e9cadb34
Bruno Azoulay <azoulaybruno@gmail.com>
2019-04-27 14:43:14 +0200

    add openjdk

--- install.sh
+++ install.sh
@@ -17,22 +17,13 @@ curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fi
 # reset terminal
 reset
 
-# add fisher package
-fisher add fishpkg/fish-prompt-metro
-fisher add franciscolourenco/done
-fisher
-fisher self-update
-
-# reset terminal
-reset
-
 # easy python
 sudo easy_install pip
 
 # install software with cask
 brew cask install \
     discord appcleaner hyper docker java postman iina sequel-pro gitkraken \
-    visual-studio-code google-chrome alfred sketch code-notes \
+    visual-studio-code google-chrome alfred sketch code-notes robo-3t adoptopenjdk \
     webtorrent dash teamviewer visual-studio iterm2 insomnia dockstation \
     ndm java imageoptim onyx sketchpacks balenaetcher vmware-fusion jetbrains-toolbox
 
@@ -44,3 +35,16 @@ yarn global add tslint jshint eslint sass-lint webpack \
 
 # install package php
 sudo pecl install xdebug
+
+# reset terminal
+reset
+
+# add fisher package
+fisher add fishpkg/fish-prompt-metro
+fisher add franciscolourenco/done
+fisher
+fisher self-update
+
+# reset
+curl -Lo ~/.config/fish/functions/humanize_duration.fish --create-dirs https://raw.githubusercontent.com/fishpkg/fish-humanize-duration/master/humanize_duration.fish
+curl -Lo ~/.config/fish/conf.d/done.fish --create-dirs https://raw.githubusercontent.com/franciscolourenco/done/master/conf.d/done.fish
